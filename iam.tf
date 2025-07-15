d577335e202440d6fb8a4c9039a258402728e71f
Danny <dfarkouh@gmail.com>
2025-07-15 14:56:10 -0400

    Add role policy attachment and created instance profile

--- iam.tf
+++ iam.tf
@@ -30,3 +30,15 @@ resource "aws_iam_policy" "ec2_s3_upload_policy" {
     }]
   })
 }
+
+# Attach role to policy 
+resource "aws_iam_role_policy_attachment" "ec2_s3_upload_attachment" {
+  role       = aws_iam_role.ec2_s3_upload_role.name
+  policy_arn = aws_iam_policy.ec2_s3_upload_policy.arn
+}
+
+# Instance profile 
+resource "aws_instance_profile" "ec2_s3_upload_profile" {
+  name = "ec2-s3-upload-profile"
+  role = aws_iam_role.ec2_s3_upload_role.name
+}

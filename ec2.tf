9ea7870f2ee670d0452e59caf75a818d47a21643
Danny <dfarkouh@gmail.com>
2025-07-15 15:17:12 -0400

    Create basic EC2 Instance

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,15 @@
 
 # EC2 Instance 
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-0c101f26f147fa7fd"
-  instance_type = "t2.micro"
+  ami                         = "ami-0c101f26f147fa7fd"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public.id
+  security_groups             = []
+  iam_instance_profile        = aws_iam_instance_profile.ec2_s3_upload_profile.name
+  associate_public_ip_address = true
 
+  tags = {
+    Name = "ec2-instance"
+  }
 }
+

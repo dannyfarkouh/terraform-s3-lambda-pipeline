23ff59ef449d6c6c5ec7319e69d206c360b951cb
Danny <dfarkouh@gmail.com>
2025-07-15 14:48:50 -0400

    Add IAM Policy for EC2 instance

--- iam.tf
+++ iam.tf
@@ -1,4 +1,5 @@
 # Policies and Rules so that the EC2 instance is able to push files into the s3 bucket 
+# Role
 resource "aws_iam_role" "ec2_s3_upload_role" {
   name = "ec2-s3-upload-role"
 
@@ -10,6 +11,22 @@ resource "aws_iam_role" "ec2_s3_upload_role" {
         Service = "ec2.amazonaws.com"
       }
       Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Policy 
+resource "aws_iam_policy" "ec2_s3_upload_policy" {
+  name = "ec2-s3-upload-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:PutObject"
+      ]
+      Resource = "${aws_s3_bucket.data_s3_bucket.arn}/*"
     }]
   })
 }
